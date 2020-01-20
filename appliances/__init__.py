f1daad605a42d992b6b0ab07607ad6f2cef8a312
Sophia Hoffman <sophiahoffman@hotmail.com>
2020-01-20 15:43:17 -0600

    saved file

--- appliances/__init__.py
+++ appliances/__init__.py
@@ -5,4 +5,3 @@ from appliances.kitchen.utility.refrigerator import Refrigerator
 from appliances.laundry.dryer import Dryer
 from appliances.laundry.washer import Washer
 
-from .appliance import Appliance
